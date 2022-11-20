4e12d92338f1aa8f37f1036ba8477a6e0c26e845
Marzed <marzed.code@gmail.com>
2022-11-20 18:36:10 -0500

    remove console.log

--- src/temporal/activities/user-tokens/create-or-update.ts
+++ src/temporal/activities/user-tokens/create-or-update.ts
@@ -16,26 +16,20 @@ export async function updateUserTokens(
     { upsert: true }
   )
     .then((result) => {
-      console.log('result1', result);
       return StatusSuccess;
     })
     .catch((error) => {
-      console.log('error >>', error);
       return StatusError;
     });
-
-  console.log('result', result);
   return StatusSuccess;
 }
 
 export async function getUserTokensByIDAndRefresh(id: string, refresh: string): Promise<boolean> {
   return await UserTokens.deleteOne({ _id: id, refresh })
     .then((result) => {
-      console.log('resultx:', result);
       return result.deletedCount !== 0;
     })
     .catch((ERR) => {
-      console.log('ERR:', ERR);
       return false;
     });
 }
