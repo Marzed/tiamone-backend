1fd243c93ffbc86166f06190e2961116a9a85ee9
Marzed <marzed.code@gmail.com>
2022-11-20 18:42:29 -0500

    change back 'index' to 'app'

--- src/index.ts
+++ src/index.ts
@@ -4,31 +4,31 @@ import userRoutes from './routes/user';
 import authRoutes from './routes/auth';
 import * as send from './constants/response';
 const express = require('express');
-const index = express();
+const app = express();
 import Logger from './logger/logger';
 
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const StartServer = () => {
-  index.use(cors());
-  index.use(bodyParser.json());
-  index.use(bodyParser.urlencoded({ extended: true }));
+  app.use(cors());
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true }));
 
-  index.get('/', (req: any, res: any, next: any) => {
+  app.get('/', (req: any, res: any, next: any) => {
     send.Success(res);
     return;
   });
 
-  index.use('/api/user', userRoutes);
-  index.use('/api/auth', authRoutes);
+  app.use('/api/user', userRoutes);
+  app.use('/api/auth', authRoutes);
 
   // unknown path
-  index.use((req: any, res: any, next: any) => {
+  app.use((req: any, res: any, next: any) => {
     return send.Unknown(res);
   });
 
-  index.listen(config.server.port, () =>
+  app.listen(config.server.port, () =>
     Logger.info('Startup', 'Start API on port: ' + config.server.port)
   );
 };
